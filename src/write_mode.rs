bc113ae104ebcb26f0a458d054710aab78fe160f
amigin <amigin@gmail.com>
2023-09-14 13:54:07 +0300

    Added more logs

--- src/write_mode.rs
+++ src/write_mode.rs
@@ -1,19 +1,18 @@
 use std::sync::atomic::AtomicI64;
 
+#[derive(Debug, Clone)]
 pub enum WriteMode {
     Unknown,
     Grpc,
     Http,
 }
 
-
-impl WriteMode{
-    pub fn is_unknown(&self)->bool{
-        match self{
+impl WriteMode {
+    pub fn is_unknown(&self) -> bool {
+        match self {
             WriteMode::Unknown => true,
             _ => false,
         }
-
     }
 }
 pub struct WriteModeKeeper {
@@ -36,6 +35,7 @@ impl WriteModeKeeper {
     }
 
     pub fn set_write_mode(&self, mode: WriteMode) {
+        println!("Telemetry mode is switched to {:?}", mode);
         match mode {
             WriteMode::Grpc => self.mode.store(1, std::sync::atomic::Ordering::Relaxed),
             WriteMode::Http => self.mode.store(2, std::sync::atomic::Ordering::Relaxed),
